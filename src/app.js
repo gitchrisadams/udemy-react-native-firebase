e12c93b958965c197d8c1dad330c31814b621ad1
Christopher Adams <chrismichaeladams@gmail.com>
2017-08-19 14:42:00 -0400

    Finished lecture 61

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp({
+	    apiKey: "AIzaSyBi56VXE1D0gDyWH8ENtyvMrMgL1KVcV6E",
+	    authDomain: "udemy-react-native-auth-53d16.firebaseapp.com",
+	    databaseURL: "https://udemy-react-native-auth-53d16.firebaseio.com",
+	    projectId: "udemy-react-native-auth-53d16",
+	    storageBucket: "udemy-react-native-auth-53d16.appspot.com",
+	    messagingSenderId: "930663721876"
+		});
+	}
+
 	render() {
 		return (
 			<View>
-				<Text>An App!</Text>
+				<Header headerText="Authentication" />
+				<LoginForm />
 			</View>
 		);
 	}
---
+++ src/components/common/CardSection.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const CardSection = (props) => {
+	return (
+		<View style={styles.containerStyle}>
+			{props.children}
+		</View>
+	);
+};
+
+{/* Styles */}
+{/* flex-direction set to row so flows left to right
+	* justify-content to flex-start so aligned to left side.
+*/}
+const styles = {
+	containerStyle: {
+		borderBottomWidth: 1,
+		padding: 5,
+		backgroundColor: '#fff',
+		justifyContent: 'flex-start',
+		flexDirection: 'row',
+		borderColor: '#ddd',
+		position: 'relative'
+	}
+};
+
+export { CardSection };
