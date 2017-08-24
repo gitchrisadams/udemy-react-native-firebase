24254c255dd8b7d45ffb7dcae780d87f8a3bb0c4
Christopher Adams <chrismichaeladams@gmail.com>
2017-08-24 07:05:02 -0400

    Finish lecture 70, Login with firebase and auth failure message

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,39 @@
 import React, {Component} from 'react';
+import {Text} from 'react-native';
+import firebase from 'firebase';
 import {Button, Card, CardSection, Input} from './common';
 
-{/* value and onChangeText are passed to Input as prop */}
+{/* 
+		email, password, and error are initialized as state.
+
+		onButtonPress() is action to take when button pressed.
+		The state is destructured to email and password.
+
+		email and password are passed to firebase for authentication.
+		.catch means user does not exist so create the user instead of login.
+		The nested .catch shows login errors.
+
+		value and onChangeText are passed to Input as prop 
+
+		for secureTextEntry, we don't need the =true part if its true.
+
+		Error message is displayed in the {this.state.error} text area.
+*/}
 class LoginForm extends Component {
-	state = {email: '' };
+	state = {email: '', password: '', error: ''};
+
+	onButtonPress() {
+		const {email, password} = this.state;
+
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.catch(() => {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.catch(() => {
+						this.setState({error: 'Authentication failed.'});
+					});
+			});
+	}
+
 	render() {
 		return (
 			<Card>
@@ -15,10 +45,24 @@ class LoginForm extends Component {
 						onChangeText={email => this.setState({email})}
 					/>
 				</CardSection>
-				<CardSection />
+
 
 				<CardSection>
-					<Button>
+					<Input
+						secureTextEntry
+						placeholder="password"
+						label="Password"
+						value={this.state.password}
+						onChangeText={password => this.setState({password})}
+					/>
+				</CardSection>
+
+				<Text style={styles.errorTextStyle}>
+					{this.state.error}
+				</Text>
+
+				<CardSection>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Login
 					</Button>
 				</CardSection>
@@ -27,4 +71,23 @@ class LoginForm extends Component {
 	}
 }
 
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+};
+
+
 export default LoginForm;
+
+
+
+
+
+
+
+
+
+
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -8,12 +8,13 @@ import {TextInput, View, Text} from 'react-native';
 		autocorrect={false} tells ios not to use autoCorrect, since this 
 		is an email address and autoCorrect is not necessary. 
 */}
-const Input = ({label, value, onChangeText, placeholder}) => {
+const Input = ({label, value, onChangeText, placeholder, secureTextEntry}) => {
 	const { inputStyle, labelStyle, containerStyle } = styles;
 	return (
 		<View style={containerStyle}>
 			<Text style={labelStyle}>{label}</Text>
 			<TextInput
+				secureTextEntry={secureTextEntry}
 				placeholder={placeholder}
 				autoCorrect={false}
 				style={inputStyle}
